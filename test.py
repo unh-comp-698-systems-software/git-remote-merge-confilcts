8dde0e5885ac160686b28b1b611c2c15b9897da1
Dan Couture <mathyourlife@gmail.com>
2017-03-20 19:09:41 -0400

    Template test file

---
+++ test.py
@@ -0,0 +1,16 @@
+import unittest
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_something(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
